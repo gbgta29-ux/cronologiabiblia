c5a9d688d96c676a2ea69a3eb6076c4fe1c716aa
Gabriel Gomes <gbgta29@gmail.com>
2025-11-07 14:50:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2102878942-2b4ac",
+  "appId": "1:660440704401:web:201957e6cdb03f1e02a630",
+  "apiKey": "AIzaSyCdCwtzV47M8Xf2mfdK8__7CkUTYs0HvTs",
+  "authDomain": "studio-2102878942-2b4ac.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "660440704401"
+};
